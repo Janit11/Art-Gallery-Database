176bd412e03041472fa4e365683b49496a03b796
rinaryu <rina.ryuRR@gmail.com>
2022-07-12 23:47:50 -0700

    Create index.php
    Adding new file

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+	include_once 'dbh.inc.php';
+	
+	$conn = OpenCon();
+	echo "Connection Successful :)";
+	
+	CloseCon($conn);
+	
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+<head>
+<body>
+
+</body>
+</html>
