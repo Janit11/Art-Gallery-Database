eb9568b8499331953f0a4ee4df13e4fc64fdfa71
Janit11 <kjanit15@gmail.com>
2022-07-26 14:17:45 -0700

    added artForms too artworks

--- artForms.php
+++ artForms.php
@@ -5,18 +5,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Artworks</title>
+	<title>ArtForms</title>
     <link rel="stylesheet" href="style2.css">
 
 <head>
 <body>
-    <h1>Artworks</h1>
-    <div class="wrapper">
-        <p><a class="active" href="artForms.php">View Art forms</a></p>
-    </div>
+    <h1>Art Forms</h1>
+
 	<?php
         $conn = OpenCon();
-        $sql = "SELECT * FROM Artwork_Create_IsIn;";
+        $sql = "SELECT * FROM BelongsTo";
         $result = mysqli_query($conn, $sql);
         $resultCheck = mysqli_num_rows($result);
 
@@ -24,10 +22,7 @@
         if ($resultCheck > 0) {
             while ($row = mysqli_fetch_assoc($result)) { //"mysqli_fetch_assoc" fetches all the info from $result (php function)
                 echo "Art ID: {$row['artID']} <br>
-                    Artist ID: {$row['artistID']} <br>
-                    Museum ID: {$row['mID']} <br>
-                    Title: {$row['title']} <br>
-                    Material ID: {$row['material']} ";
+                    Art Name: {$row['artName']} ";
                 echo "<br> <br>"; 
             }
         }
