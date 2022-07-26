ab2d16e7885f58236495db650ee4c96cfd329f40
Janit11 <kjanit15@gmail.com>
2022-07-26 14:50:06 -0700

    Added functionality to join file

--- join.php
+++ join.php
@@ -1,10 +1,12 @@
 <?php
-
     include_once 'include/dbh.inc.php';
     require 'header.php';
     $conn = OpenCon();
     $sql1 = "SHOW COLUMNS FROM Artwork_Create_IsIn";
-    $result = $conn->query($sql);
+    $result1 = $conn->query($sql1);
+    $sql2 = "SHOW COLUMNS FROM BelongsTo ";
+    $result2 = $conn->query($sql2);
+
 ?>
 
 <!DOCTYPE html>
@@ -24,3 +26,56 @@
         }
     </style>
 <head>
+<body>
+    <h1>Join Query</h1>	
+    <p>Join the columns from both tables to get the desired result. </p>
+    <form method = "POST">
+    <select name = "colName">
+        <optgroup label="first-choice">
+            <option> -- Select a column -- </option>
+            <?php 
+                while ($row = mysqli_fetch_array($result1)) {
+                    ?>
+                    <option value="<?php echo $row["Field"]; ?> " > <?php echo $row["Field"]; ?> </option>
+                    
+
+            <?php
+            }
+        ?>
+        <optgroup label="second-choice">
+        <option> -- Select a column -- </option>
+            <?php 
+                while ($row = mysqli_fetch_array($result2)) {
+                    ?>
+                    <option value="<?php echo $row["Field"]; ?> " > <?php echo $row["Field"]; ?> </option>
+                   
+            <?php
+            }
+        ?>
+    <select>
+        <input type = "submit" name = "Submit" value = "Choose" />
+    <?php
+        if(isset($_POST["Submit"])) {
+
+        
+            $colName = $_POST["colName"];
+            echo " <br>"; 
+            echo $colName;
+
+            $sql2 = "SELECT '$colName' FROM Curator";
+            $result2 = $conn->query($sql2);
+            $resultCheck2 = mysqli_num_rows($result2);
+            
+            if ($resultCheck2 > 0) {
+                while ($row = mysqli_fetch_array($result2)) { 
+                    echo $row;
+                        
+                    echo " <br>"; 
+                }
+            }
+        }
+    ?>
+    <form>
+    
+</body>
+</html> 
