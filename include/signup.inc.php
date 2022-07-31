b94d5c8f7efb3eadaa8768cb1494d2a2a2ab0bc6
rinaryu <rina.ryuRR@gmail.com>
2022-07-31 14:45:28 -0700

    Deleted line of code and added logout
    Deleted the hashing for the password when user creates a new account.

--- include/signup.inc.php
+++ include/signup.inc.php
@@ -50,8 +50,7 @@ if (isset($_POST['signup-submit'])) { //checking that the user has clicked the s
                     exit();
                 }
                 else {
-                    $hashedPwd = password_hash($password, PASSWORD_DEFAULT); //hashing the password so it's not visible
-                    mysqli_stmt_bind_param($stmt, "sss", $username, $email, $hashedPwd);
+                    mysqli_stmt_bind_param($stmt, "sss", $username, $email, $password);
                     mysqli_stmt_execute($stmt);
                     header("Location: ../signup.php?signup=success");
                     exit();
